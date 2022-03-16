aa17c5c2c4d570853479e7d3b56e679fec2af582
Md. Sourov Mia <72209500+sourov15-13526@users.noreply.github.com>
2022-03-16 00:10:03 -0700

    Add files via upload

---
+++ Loop/Loop ex 3.py
@@ -0,0 +1,5 @@
+sum1 = 0
+n = int(input("Please enter number "))
+for i in range(1, n + 1, 1):
+    sum1 += i
+print("Sum is: ", sum1)
---
+++ Loop/Loop ex 6.py
@@ -0,0 +1,6 @@
+num = 75869
+count = 0
+while num != 0:
+    num //= 10
+    count+= 1
+print("Total digits are: ", count)
